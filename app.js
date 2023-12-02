0d836cb28935ab3e2bfb1e54838fa2e8f6f77c55
michael mbugua <mikembugua960@gmail.com>
2023-12-02 21:20:09 +0300

    define a db variable to store the connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql");
 
+// connecting to a mysql database
+const db = mysql.createConnection({});
 const app = express();
 const port = 5000;
 
